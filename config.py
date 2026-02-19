b19326a551444b3a15a251992173bef2427b504d
unknown <User@DESKTOP-0NVRCO0.amikonline.com>
2026-02-19 20:11:11 +0200

    testing the CI

--- config.py
+++ config.py
@@ -6,5 +6,4 @@ load_dotenv()
 
 MODEL_NAME = "openai/gpt-oss-20b"
 TEMPERATURE = 0.0
-#GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-GROQ_API_KEY = "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"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
